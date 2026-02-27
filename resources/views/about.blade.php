3317764357c9f203e778345e30af2ac7295efb5d
Ekoramadhan13 <enugraha411@gmail.com>
2026-02-27 14:26:18 +0700

    menganti isi code di file about.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Nama: Eko Ramadhan Nugroho") }}
+                    <br>
+                    {{ __("NIM: 20230140114") }}
+                    <br>
+                    {{ __("Prodi: Teknologi Informasi") }}
+                    <br>
+                    {{ __("Hobi: Fishing") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
